30582642dd799a75631ba2b02bcf6423942b8a9d
yuncai <wuyuncai@standard-robots.com>
2020-08-05 21:59:47 +0800

    begin the hybird A star program.

---
+++ include/hybird_A_star.h
@@ -0,0 +1,18 @@
+#ifndef HYBIRD_A_STAR_H_
+#define HYBIRD_A_STAR_H_
+
+#include <iostream>
+
+#include <ros/ros.h>
+
+using namespace std;
+
+class HybirdAStar {
+public:
+    HybirdAStar();
+    ~HybirdAStar();
+
+private:
+};
+
+#endif
---
+++ src/hybird_A_star.cpp
@@ -0,0 +1,6 @@
+#include "hybird_A_star.h"
+
+int main(int argc, char** argv) {
+    cout << "begin the program." << endl;
+    return 0;
+}
